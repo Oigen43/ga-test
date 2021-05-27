27da0e3fc991971012c7c62e5e22e0cb9503da57
Evgenii <oigen43@icloud.com>
2021-05-27 13:59:24 +0000

    Update README.md

--- api/src/app.constants.js
+++ api/src/app.constants.js
@@ -11,4 +11,3 @@ module.exports = {
   },
   TOKEN_SECURITY_LENGTH: 32,
 };
-22222
--- api/src/logger.js
+++ api/src/logger.js
@@ -3,4 +3,3 @@ const { createConsoleLogger } = require('@paralect/common-logger');
 const config = require('config');
 
 module.exports = createConsoleLogger({ isDev: config.isDev });
-12312312
